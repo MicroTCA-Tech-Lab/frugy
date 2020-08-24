c4bd143b70eb0b46fdbc1e5c90845c36f31451c1
Patrick Huesmann <patrick.huesmann@desy.de>
2020-08-24 14:41:11 +0200

    Added ChassisInfo

--- frugy/areas.py
+++ frugy/areas.py
@@ -1,10 +1,15 @@
-from frugy.types import FruArea, FixedField
+from frugy.types import FruArea, FixedField, StringField
 
 
-class FruCommonHeader(FruArea):
-    _format_version = 1
+_format_version = 1
+_format_field = ('format_version',
+                 FixedField('u4u4', value=(0, _format_version)))
+_delimiter_field = ('delimiter', FixedField('u8', value=0xc1))
+
+
+class CommonHeader(FruArea):
     _schema = [
-        ('format_version', FixedField('u4u4', value=(0, _format_version))),
+        _format_field,
         ('internal_use_offs', FixedField('u8', value=0)),
         ('chassis_info_offs', FixedField('u8', value=0)),
         ('board_area_offs', FixedField('u8', value=0)),
@@ -22,3 +27,15 @@ class FruCommonHeader(FruArea):
             self._set(key, int(value / 8))
         else:
             self._set(key, value)
+
+
+class ChassisInfo(FruArea):
+    _schema = [
+        _format_field,
+        ('area_length', FixedField('u8')),
+        ('chassis_type', FixedField('u8')),
+        ('chassis_part_number', StringField),
+        ('chassis_serial_number', StringField),
+        # TODO: do we need custom chassis info fields?
+        _delimiter_field
+    ]
