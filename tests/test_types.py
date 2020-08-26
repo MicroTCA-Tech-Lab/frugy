9ae6f536664ea97753553b8c73f6605a2ab529f2
Patrick Huesmann <patrick.huesmann@desy.de>
2020-08-26 12:18:25 +0200

    removed FooArea test (we have real IPMI types now)

--- tests/test_types.py
+++ tests/test_types.py
@@ -73,38 +73,5 @@ class TestString(unittest.TestCase):
         self.assertEqual(tmp2.value, 'IPMI HELLO WORLD')
 
 
-class FooArea(FruArea):
-    _schema = [
-        ('foo', FixedField('u4u4', value=(5, 7))),
-        ('bar', FixedField('u8', value=42)),
-        ('baz', FixedField('u16', value=0x1337)),
-        ('foobar', FixedField('u16', value=0)),
-    ]
-
-    def __get_foobar(self):
-        return self._get('foobar') ^ 0xffff
-
-    def __set_foobar(self, value):
-        self._set('foobar', value ^ 0xffff)
-
-
-class TestFruArea(unittest.TestCase):
-    def test_dict(self):
-        foo = FooArea()
-        ref = {
-            'foo': [5, 7],
-            'bar': 42,
-            'baz': 0x1337,
-            'foobar': 0
-        }
-        self.assertEqual(foo.__repr__(), ref.__repr__())
-
-    def test_serialize(self):
-        foo = FooArea()
-        self.assertEqual(foo.size_payload(), 8)
-        self.assertEqual(foo.size_total(), 16)
-        self.assertEqual(foo.serialize(), b'\x01\x02W*7\x13\x00\x00\x00\x00\x00\x00\x00\x00\x002')
-
-
 if __name__ == '__main__':
     unittest.main()
