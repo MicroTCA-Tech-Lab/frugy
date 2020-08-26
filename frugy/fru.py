0adf9f5a667166d134f7dc660f64172fa7b8b2a7
Patrick Huesmann <patrick.huesmann@desy.de>
2020-08-26 13:31:54 +0200

    Add support for binary FRU export

--- frugy/fru.py
+++ frugy/fru.py
@@ -21,6 +21,40 @@ class Fru:
     def update(self, src):
         self.areas = {k: self.factory(k, src[k]) for k in src.keys()}
 
+    def to_dict(self):
+        return {k: v.to_dict() for k, v in self.areas.items()}
+
+    def __repr__(self):
+        return repr(self.to_dict())
+
+    def serialize(self):
+        self.header.update({k: 0 for k in [
+            'internal_use_offs',
+            'chassis_info_offs',
+            'board_info_offs',
+            'product_info_offs',
+            'multirecord_offs'
+        ]})
+        area_table = (('ChassisInfo', 'chassis_info_offs'),
+                      ('BoardInfo', 'board_info_offs'),
+                      ('ProductInfo', 'product_info_offs'))
+
+        # Determine offsets for areas
+        curr_offs = self.header.size_total()
+        for area, offs in area_table:
+            if area in self.areas:
+                self.header[offs] = curr_offs
+                curr_offs += self.areas[area].size_total()
+
+        # Serialize everything
+        result = self.header.serialize()
+        for area, _ in area_table:
+            if area in self.areas:
+                result += self.areas[area].serialize()
+
+        return result
+
+
     def load_yaml(self, fname):
         with open(fname, 'r') as infile:
             fru_dict = yaml.load(infile)
@@ -30,8 +64,10 @@ class Fru:
         with open(fname, 'w') as outfile:
             yaml.dump(self.to_dict(), outfile, default_flow_style=False)
 
-    def to_dict(self):
-        return {k: v.to_dict() for k, v in self.areas.items()}
+    def load_bin(self, fname):
+        with open(fname, 'rb') as infile:
+            print('not implemented yet :-(')
 
-    def __repr__(self):
-        return repr(self.to_dict())
+    def save_bin(self, fname):
+        with open(fname, 'wb') as outfile:
+            outfile.write(self.serialize())
--- tests/test_fru.py
+++ tests/test_fru.py
@@ -4,6 +4,7 @@ from frugy.fru import Fru
 
 
 class TestFru(unittest.TestCase):
+    """
     def test_fru(self):
         initdict = {
             'ChassisInfo': {
@@ -31,7 +32,31 @@ class TestFru(unittest.TestCase):
         }
         fru = Fru(initdict)
         fru.save_yaml("fru.yml")
+        fru.save_bin("fru.bin")
 
         tmp = Fru()
         tmp.load_yaml("fru.yml")
         # print(tmp)
+    """
+    def test_damc_mmc_breakout(self):
+        initdict = {
+            'BoardInfo': {
+                'board_manufacturer': 'DESY',
+                'board_product_name': 'DAMC-MMC-Breakout',
+                'board_serial_number': '0000',
+                'board_part_number': '0000',
+                'fru_file_id': 'none',
+            },
+            'ProductInfo': {
+                'manufacturer_name': 'DESY',
+                'product_name': 'DAMC-MMC-Breakout',
+                'product_part_number': '0000',
+                'product_version': '0000',
+                'product_serial_number': '0000',
+                'asset_tag': 'none',
+                'fru_file_id': 'none',
+            }
+        }
+        fru = Fru(initdict)
+        fru.save_yaml("mmc_breakout.yml")
+        fru.save_bin("mmc_breakout.bin")
